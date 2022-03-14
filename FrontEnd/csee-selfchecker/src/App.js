2518b3e98c25b1667abacb7013b77cabd6e492d7
ByungwoongAn <gomster96@gmail.com>
2022-03-14 17:52:55 +0900

    [#2] : StyledComponents 적용
    - StyledComponents 를 적용하고, 해당 사용예시를 App.js 에 놓았다.

--- FrontEnd/csee-selfchecker/src/App.js
+++ FrontEnd/csee-selfchecker/src/App.js
@@ -1,7 +1,27 @@
 import React from "react";
+import styled from "styled-components";
+
+const Test = styled.div`
+    color: red;
+`;
+
+const ButtonTest = styled.button`
+    width: 500px;
+    height: 300px;
+    border: 1px solid blue;
+    background-color: "red";
+`;
 
 function App() {
-    return <div>안녕하세여</div>;
+    const hello = () => {
+        console.log("hihihi");
+    };
+    return (
+        <>
+            <Test>안녕하세요~~</Test>
+            <ButtonTest onClick={hello}>이렇게 버튼을 만듭니다</ButtonTest>
+        </>
+    );
 }
 
 export default App;
