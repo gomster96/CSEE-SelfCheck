5fe37e29b5f0c678c5d354902d07a85cd9aa63f1
ByungwoongAn <gomster96@gmail.com>
2022-03-14 18:15:04 +0900

    [#1] 백엔드 프로젝트 프로젝트 생성
    - Spring Boot 로 벡엔드 프로젝트를 생성하였다.

---
+++ BackEnd/src/main/java/com/example/cseeselfcheck/CseeSelfCheckApplication.java
@@ -0,0 +1,13 @@
+package com.example.cseeselfcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CseeSelfCheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CseeSelfCheckApplication.class, args);
+    }
+
+}
