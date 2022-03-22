571ec94c029d2d36c56c959be09b003c57112124
Lee <21801049@handong.edu>
2022-03-22 20:09:01 +0900

    [#25] feat: 페이지 전체 css 수정

--- FrontEnd/csee-selfchecker/src/pages/Admin/index.js
+++ FrontEnd/csee-selfchecker/src/pages/Admin/index.js
@@ -1,24 +1,81 @@
 import React from 'react';
 import styled from 'styled-components';
+import background from '../../asset/img/backgroundImg.png';
 import AdminTable from './AdminTable';
 import Footer from './Footer';
 import HeaderFilter from './HeaderFIlter';
 import SearchBar from './SearchBar';
 
-const AdminLayout = styled.div`
-  margin-left: 20vw;
-  margin-right: 20vw;
+const calcWidthPercent = (span) => {
+  if (!span) return;
+
+  const width = (span / 12) * 100;
+  return width;
+};
+
+const BREAK_POINT_MOBILE = 768;
+const BREAK_POINT_TABLET = 992;
+const BREAK_POINT_PC = 1200;
+
+const ContainerDiv = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: #eff0f2;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-image: url(${background});
+
+  width: ${({ xs }) => (xs ? `${calcWidthPercent(xs)}%` : `100%`)};
+  @media only screen and (min-width: ${BREAK_POINT_MOBILE}px) {
+    width: ${({ sm }) => sm && `${calcWidthPercent(sm)}%`};
+  }
+  @media only screen and (min-width: ${BREAK_POINT_TABLET}px) {
+    width: ${({ md }) => md && `${calcWidthPercent(md)}%`};
+  }
+  @media only screen and (min-width: ${BREAK_POINT_PC}px) {
+    width: ${({ lg }) => lg && `${calcWidthPercent(lg)}%`};
+  }
+`;
+
+const LoginFormDiv = styled.div`
+  background: #f5f5f5;
+  height: 85vh;
+  display: inline-block;
+  flex-direction: row;
+  box-shadow: 10px black;
+  border-radius: 40px;
+  text-align: center;
+
+  /* 그림자 */
+  -webkit-box-shadow: 27px 43px 43px -26px rgba(89, 89, 89, 0.39);
+  -moz-box-shadow: 27px 43px 43px -26px rgba(89, 89, 89, 0.39);
+  box-shadow: 27px 43px 43px -26px rgba(89, 89, 89, 0.39);
+
+  width: ${({ xs }) => (xs ? `${calcWidthPercent(xs)}%` : `70vw`)};
+  padding: 1%;
+  @media only screen and (min-width: ${BREAK_POINT_MOBILE}px) {
+    width: ${({ sm }) => sm && `${calcWidthPercent(sm)}%`};
+  }
+  @media only screen and (min-width: ${BREAK_POINT_TABLET}px) {
+    width: ${({ md }) => md && `${calcWidthPercent(md)}%`};
+  }
+  @media only screen and (min-width: ${BREAK_POINT_PC}px) {
+    width: ${({ lg }) => lg && `${calcWidthPercent(lg)}%`};
+  }
 `;
 
 export default function Admin() {
   return (
     <>
-      <AdminLayout>
-        <HeaderFilter />
-        <SearchBar />
-        <AdminTable />
-        <Footer />
-      </AdminLayout>
+      <ContainerDiv>
+        <LoginFormDiv>
+          <HeaderFilter />
+          <SearchBar />
+          <AdminTable />
+          <Footer />
+        </LoginFormDiv>
+      </ContainerDiv>
     </>
   );
 }
