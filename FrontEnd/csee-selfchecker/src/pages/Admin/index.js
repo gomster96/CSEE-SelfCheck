d20190a240df05ce70d6b3d2498d464859ceb41d
Vinodi Lee <83273272+skLee-HGU@users.noreply.github.com>
2022-03-24 21:10:25 +0900

    [#29] feat: 관리자 회원가입 페이지, 전체 css 및 화면 규격 수정 (#35)

--- FrontEnd/csee-selfchecker/src/pages/Admin/index.js
+++ FrontEnd/csee-selfchecker/src/pages/Admin/index.js
@@ -40,7 +40,8 @@ const ContainerDiv = styled.div`
 
 const LoginFormDiv = styled.div`
   background: #f5f5f5;
-  height: 85vh;
+  width: 1050px;
+  height: 700px;
   display: inline-block;
   flex-direction: row;
   box-shadow: 10px black;
