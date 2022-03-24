b96df31eca3c4f0b7ec89f5b4f5a5b9deac7fe87
gomster <gomster96@gmail.com>
2022-03-24 15:15:11 +0900

    [#27] feat : exception 생성
    - excel파일이 아닐경우 발생하는 exception (ImportException)
    - 파일을 stream으로 열때 오류발생시 발생하는 (OpenFIle Exception)
    - import 하는 파일의 양식이 잘못될 경우의 DataFormatException을 만들어 주었다.

--- BackEnd/src/main/java/com/example/cseeselfcheck/exception/GlobalExceptionHandler.java
+++ BackEnd/src/main/java/com/example/cseeselfcheck/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.example.cseeselfcheck.exception;
 
+import java.io.IOException;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(CseeCheckerException.class)
-    public ResponseEntity<ExceptionResponse> rebonException(CseeCheckerException e) {
+    public ResponseEntity<ExceptionResponse> cseeCheckerException(CseeCheckerException e) {
         return ResponseEntity.status(e.getHttpStatus())
                              .body(new ExceptionResponse(e.getMessage()));
     }
--- BackEnd/src/main/java/com/example/cseeselfcheck/exception/referenceuser/ReferenceUserImportException.java
+++ BackEnd/src/main/java/com/example/cseeselfcheck/exception/referenceuser/ReferenceUserImportException.java
@@ -4,6 +4,6 @@ import org.springframework.http.HttpStatus;
 
 public class ReferenceUserImportException extends ReferenceUserException{
     public ReferenceUserImportException(){
-        super("잘못된 양식입니다.", HttpStatus.BAD_REQUEST);
+        super("잘못된 양식의 파일입니다.", HttpStatus.BAD_REQUEST);
     }
 }
