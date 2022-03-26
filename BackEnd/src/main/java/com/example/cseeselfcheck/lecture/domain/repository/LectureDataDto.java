49898114c4e08a530ecfcf484118dd98caee2b4c
Jueun <kje9862@naver.com>
2022-03-27 00:07:59 +0900

    12345

---
+++ BackEnd/src/main/java/com/example/cseeselfcheck/lecture/domain/repository/LectureDataDto.java
@@ -0,0 +1,9 @@
+//package com.example.cseeselfcheck.lecture.domain.repository;
+//
+//public interface LectureDataDto {
+//    String getLectureName();
+//    int getDesignCredit();
+//    int getLecturePosition();
+//    String getOpenedYear();
+//
+//}
--- BackEnd/src/main/java/com/example/cseeselfcheck/lecture/presentation/LectureController.java
+++ BackEnd/src/main/java/com/example/cseeselfcheck/lecture/presentation/LectureController.java
@@ -1,33 +1,39 @@
-package com.example.cseeselfcheck.lecture.presentation;
-
-import java.io.IOException;
-
-import com.example.cseeselfcheck.exception.common.ExcelOpenFileException;
-import com.example.cseeselfcheck.lecture.application.LectureService;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-
-import lombok.RequiredArgsConstructor;
-
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/api")
-public class LectureController {
-
-    private final LectureService lectureService;
-
-    @PostMapping("/upload/lectures")
-    public ResponseEntity<Object> uploadLecture(@RequestParam("file")MultipartFile file){
-        try{
-            lectureService.createByExcel(file);
-        } catch (IOException e){
-            throw new ExcelOpenFileException();
-        }
-        return ResponseEntity.ok().body(null);
-    }
-}
+//package com.example.cseeselfcheck.lecture.presentation;
+//
+//import java.io.IOException;
+//
+//import com.example.cseeselfcheck.exception.common.ExcelOpenFileException;
+//import com.example.cseeselfcheck.lecture.application.LectureService;
+//
+//import com.example.cseeselfcheck.lecture.domain.Lecture;
+//import com.example.cseeselfcheck.lecture.domain.repository.LectureDataDto;
+//import com.example.cseeselfcheck.user.domain.dto.UserIndividualDataDto;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//import org.springframework.web.multipart.MultipartFile;
+//
+//import lombok.RequiredArgsConstructor;
+//
+//@RestController
+//@RequiredArgsConstructor
+//@RequestMapping("/api")
+//public class LectureController {
+//
+//    private final LectureService lectureService;
+//
+//    @PostMapping("/upload/lectures")
+//    public ResponseEntity<Object> uploadLecture(@RequestParam("file")MultipartFile file){
+//        try{
+//            lectureService.createByExcel(file);
+//        } catch (IOException e){
+//            throw new ExcelOpenFileException();
+//        }
+//        return ResponseEntity.ok().body(null);
+//    }
+//    @GetMapping("/lecture/{lectureId}")
+//    public ResponseEntity<Object> getUserById(@PathVariable Long lectureId){
+//        LectureDataDto lectureData = LectureService.getLectureDataById(Lecture.id);
+//
+//        return ResponseEntity.ok(lectureData);
+//    }
+//}
