ea6d02d813a5ebed955ecd1c1146c2d8af9a9e95
gomster <gomster96@gmail.com>
2022-03-26 14:18:57 +0900

    [#36] feat : Admin페이지 필터유저 가져오는 API 구현
    - Admin 페이지에서 필터 하는 기능에 따라 데이터를 가져오는 API를 구현하였다.
    - 들어가는 데이터의 꼴은 다음과 같다. 
    {
        "lectures" : [0, 2, 3],
        "semesters" : ["6학기", "7학기", "8학기"],
        "takePossible"  : true
        }

--- BackEnd/src/main/java/com/example/cseeselfcheck/user/application/UserService.java
+++ BackEnd/src/main/java/com/example/cseeselfcheck/user/application/UserService.java
@@ -1,4 +1,30 @@
 package com.example.cseeselfcheck.user.application;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.example.cseeselfcheck.admin.presentation.dto.AdminUserRequestDto;
+import com.example.cseeselfcheck.user.domain.dto.UserDataDto;
+import com.example.cseeselfcheck.user.domain.repository.UserRepository;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+    public List<UserDataDto> getFilteredUser(AdminUserRequestDto data){
+
+        List<UserDataDto> userDatas = userRepository.findUserData();
+        List<UserDataDto> filteredUsers = userDatas.stream()
+                                                   .filter(data::isUserContainLecture)
+                                                   .filter(data::isUserContainSemester)
+                                                   .filter(data::isSameTakePossible)
+                                                   .collect(Collectors.toList());
+
+        return filteredUsers;
+    }
+
 }
---
+++ BackEnd/src/main/java/com/example/cseeselfcheck/user/domain/dto/UserDataDto.java
@@ -0,0 +1,18 @@
+package com.example.cseeselfcheck.user.domain.dto;
+
+public interface UserDataDto {
+    String getStudentName();
+    String getName();
+    String getSemester();
+    String getMajorName();
+    int getResult();
+    String getTakenStatus();
+
+    default boolean isTakenLecture(int lectureIdx){
+        return getTakenStatus().charAt(lectureIdx) == '1';
+    }
+
+    default boolean isSameSemester(String semester){
+        return getSemester().equals(semester);
+    }
+}
