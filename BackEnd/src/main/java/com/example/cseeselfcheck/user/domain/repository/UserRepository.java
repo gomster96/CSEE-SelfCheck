7eda93cac67e347e7ffdbd39e9a8aa2fd6c2134d
gomster <gomster96@gmail.com>
2022-03-27 22:35:41 +0900

    [#50] feat : 관리자페이지 데이터 불러오는 API checker 기능 추가
    - 기존 API는 어떤 과목때문에 미충족인지에 대한 여부를 안나타냈기에, 이번에 관련 기능을 보강하였다.

--- BackEnd/src/main/java/com/example/cseeselfcheck/user/domain/repository/UserRepository.java
+++ BackEnd/src/main/java/com/example/cseeselfcheck/user/domain/repository/UserRepository.java
@@ -10,12 +10,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    @Query("select u.studentNumber as studentNumber, r.major.majorName as majorName, u.result as result, u.takenStatus as takenStatus, r.name as name, r.semester as semester " +
+    @Query("select u.studentNumber as studentNumber, r.major.majorName as majorName, r.major.checker as checker, u.result as result, u.takenStatus as takenStatus, r.name as name, r.semester as semester " +
             "from User u, ReferenceUser r " +
             "where u.studentNumber = r.studentNumber")
     List<UserDataDto> findUserData();
 
-    @Query("select u.studentNumber as studentNumber, r.major.majorName as majorName, u.result as result, u.takenStatus as takenStatus, r.name as name, r.semester as semester " +
+    @Query("select u.studentNumber as studentNumber, r.major.majorName as majorName, r.major.checker as checker, u.result as result, u.takenStatus as takenStatus, r.name as name, r.semester as semester " +
             "from User u, ReferenceUser r " +
             "where u.studentNumber = r.studentNumber " +
             "and ( r.studentNumber like :searchWord% or r.name like :searchWord%)")
