42b6a9bb7b9a85ded0c050be74822bff55c054c7
Vinodi Lee <83273272+skLee-HGU@users.noreply.github.com>
2022-03-31 16:51:10 +0900

    회원가입 페이지 반응형, 헤더 적용 (#68)
    * [#66] radio 버튼 수정 및 css 수정
    
    * [#66] 회원가입 페이지 반응형, 헤더 적용

--- FrontEnd/csee-selfchecker/src/pages/Login/index.js
+++ FrontEnd/csee-selfchecker/src/pages/Login/index.js
@@ -2,8 +2,10 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import logo from '../../asset/img/loginImage.png';
 import background from '../../asset/img/backgroundImg.png';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import headerImg from '../../asset/img/csee-logo-symbol.png';
 import Googlebutton from './Googlebutton';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Form, Navbar, Container } from 'react-bootstrap';
 import { Form } from 'react-bootstrap';
 import { useNavigate } from 'react-router';
 
@@ -15,35 +17,56 @@ export default function Login() {
     console.log(value);
   };
   return (
-    <ContainerDiv>
-      <LoginFormDiv>
-        <LoginFormLeft1></LoginFormLeft1>
-        <LoginFormLeft2>
-          <LoginFormLeftTitle>
-            Computer Science <br></br>And Electrical Engineering
-          </LoginFormLeftTitle>
-          <LoginFormLeftsubTitle>Welcome to our website</LoginFormLeftsubTitle>
-          <LogoImg src={logo} />
-        </LoginFormLeft2>
-        <LoginFormRight>
-          <LoginFormRightTitle>
-            <h1>Log in</h1>
-          </LoginFormRightTitle>
-          <ItemContainer>
-            <Form.Check inline label="학생" type="radio" value="0" checked={isUserSelect === '0'} onChange={handleUserChange} />
-            <Form.Check inline label="관리자" type="radio" value="1" checked={isUserSelect === '1'} onChange={handleUserChange} />
-          </ItemContainer>
-          <GoogleLoginContainer>
-            <GoogleLoginClass>
-              <Googlebutton isUserSelect={isUserSelect} setIsUserSelect={setIsUserSelect} />
-            </GoogleLoginClass>
-          </GoogleLoginContainer>
-          <LoginFormRightsubTitle>
-            <p>학교 구글 계정으로 로그인하세요.</p>
-          </LoginFormRightsubTitle>
-        </LoginFormRight>
-      </LoginFormDiv>
-    </ContainerDiv>
+    <>
+      <Navbar bg="light">
+        <Container>
+          <Navbar.Brand href="/">
+            <img alt="" src={headerImg} width="30" height="30" className="d-inline-block align-top" /> CSEE Self-Checker
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+      <ContainerDiv>
+        <LoginFormDiv>
+          <LoginFormLeft1></LoginFormLeft1>
+          <LoginFormLeft2>
+            <LoginFormLeftTitle>
+              Computer Science <br></br>And Electrical Engineering
+            </LoginFormLeftTitle>
+            <LoginFormLeftsubTitle>Welcome to our website</LoginFormLeftsubTitle>
+            <LogoImg src={logo} />
+          </LoginFormLeft2>
+          <LoginFormRight>
+            <LoginFormRightTitle>
+              <h1>Log in</h1>
+            </LoginFormRightTitle>
+            <ItemContainer>
+              <Form.Check inline label="학생" type="radio" value="0" checked={isUserSelect === '0'} onChange={handleUserChange} />
+              <Form.Check inline label="관리자" type="radio" value="1" checked={isUserSelect === '1'} onChange={handleUserChange} />
+            </ItemContainer>
+            <GoogleLoginContainer>
+              <GoogleLoginClass>
+                <Googlebutton isUserSelect={isUserSelect} setIsUserSelect={setIsUserSelect} />
+              </GoogleLoginClass>
+            </GoogleLoginContainer>
+            <LoginFormRightsubTitle>
+              <p>학교 구글 계정으로 로그인하세요.</p>
+            </LoginFormRightsubTitle>
+          </LoginFormRight>
+        </LoginFormDiv>
+      </ContainerDiv>
+      <>
+        <FooterDiv className="bg-gray">
+          <Navbar>
+            <Container>
+              <Footer>
+                <FooterTextLayout>©WALAB 2022 </FooterTextLayout>
+                <FooterTextLayout> 안병웅, 이선경, 김주은</FooterTextLayout>
+              </Footer>
+            </Container>
+          </Navbar>
+        </FooterDiv>
+      </>
+    </>
   );
 }
 const calcWidthPercent = (span) => {
@@ -52,14 +75,13 @@ const calcWidthPercent = (span) => {
   const width = (span / 12) * 100;
   return width;
 };
-
 const BREAK_POINT_MOBILE = 768;
 const BREAK_POINT_TABLET = 992;
 const BREAK_POINT_PC = 1200;
 
 const ContainerDiv = styled.div`
   width: 100vw;
-  height: 100vh;
+  height: 90vh;
   background: #eff0f2;
   display: flex;
   justify-content: center;
@@ -80,7 +102,7 @@ const ContainerDiv = styled.div`
 const LoginFormDiv = styled.div`
   background: #f5f5f5;
   width: 1050px;
-  height: 700px;
+  height: auto;
   display: flex;
   flex-direction: row;
   box-shadow: 10px black;
@@ -144,7 +166,6 @@ const LoginFormLeft2 = styled.div`
     width: ${({ lg }) => lg && `${calcWidthPercent(lg)}%`};
   }
 `;
-
 const LoginFormLeftTitle = styled.div`
   font-family: 'RedHatDisplayRegular';
   font-size: 36px;
@@ -208,4 +229,20 @@ const ItemContainer = styled.div`
   display: flex;
   margin: 5vh 20px 10px 0px;
   padding-left: 10%;
+`;
+const FooterDiv = styled.div`
+  display: flex;
+  align-itmes: center;
+  justify-content: center;
+`;
+
+const Footer = styled.div`
+  display: flex;
+  align-itmes: center;
+`;
+
+const FooterTextLayout = styled.div`
+  color: gray;
+  font-size: 14px;
+  padding: 0.5rem;
 `;
--- FrontEnd/csee-selfchecker/src/pages/Register/Admin/index.js
+++ FrontEnd/csee-selfchecker/src/pages/Register/Admin/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
-import background from '../../../asset/img/backgroundImg.png';
 import logo from '../../../asset/img/loginImage.png';
+import background from '../../../asset/img/backgroundImg.png';
+import headerImg from '../../../asset/img/csee-logo-symbol.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Form } from 'react-bootstrap';
+import { Form, Navbar, Container, Button } from 'react-bootstrap';
 import { useNavigate, useLocation } from 'react-router';
 
 export default function AdminRegister() {
@@ -40,34 +41,55 @@ export default function AdminRegister() {
     // ... submit to API or something
   };
   return (
-    <ContainerDiv>
-      <LoginFormDiv>
-        <LoginFormLeft1></LoginFormLeft1>
-        <LoginFormLeft2>
-          <LoginFormLeftTitle>
-            Computer Science <br></br>And Electrical Engineering
-          </LoginFormLeftTitle>
-          <LoginFormLeftsubTitle>Welcome to our website</LoginFormLeftsubTitle>
-          <LogoImg src={logo} />
-        </LoginFormLeft2>
+    <>
+      <Navbar bg="light">
+        <Container>
+          <Navbar.Brand href="/">
+            <img alt="" src={headerImg} width="30" height="30" className="d-inline-block align-top" /> CSEE Self-Checker
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+      <ContainerDiv>
+        <LoginFormDiv>
+          <LoginFormLeft1></LoginFormLeft1>
+          <LoginFormLeft2>
+            <LoginFormLeftTitle>
+              Computer Science <br></br>And Electrical Engineering
+            </LoginFormLeftTitle>
+            <LoginFormLeftsubTitle>Welcome to our website</LoginFormLeftsubTitle>
+            <LogoImg src={logo} />
+          </LoginFormLeft2>
 
-        <LoginFormRight>
-          <Form onSubmit={handleSubmit}>
-            <LoginFormRightTitle>
-              <h3>Admin Information</h3>
-            </LoginFormRightTitle>
-            <Form.Group className="mb-3" style={{ marginTop: '50px' }}>
-              <Form.Control placeholder="  소속 (Department)" name="adminDepartment" onChange={handleChange} style={{ borderRadius: '20px' }} />
-            </Form.Group>
-            <Form.Group className="mb-3">
-              <Form.Control placeholder="  이름 (Name)" name="adminName" onChange={handleChange} style={{ borderRadius: '20px' }} />
-            </Form.Group>
-            <p>제출 시 관리자 승인 후 로그인 가능합니다.</p>
-            <Button as="input" type="submit" value="Submit!" style={{ width: '100%', borderRadius: '20px', background: '#2e75b6', marginTop: '10%' }} />{' '}
-          </Form>
-        </LoginFormRight>
-      </LoginFormDiv>
-    </ContainerDiv>
+          <LoginFormRight>
+            <Form onSubmit={handleSubmit}>
+              <LoginFormRightTitle>
+                <h3>Admin Information</h3>
+              </LoginFormRightTitle>
+              <Form.Group className="mb-3" style={{ marginTop: '50px' }}>
+                <Form.Control placeholder="  소속 (Department)" name="adminDepartment" onChange={handleChange} style={{ borderRadius: '20px' }} />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Control placeholder="  이름 (Name)" name="adminName" onChange={handleChange} style={{ borderRadius: '20px' }} />
+              </Form.Group>
+              <p>제출 시 관리자 승인 후 로그인 가능합니다.</p>
+              <Button as="input" type="submit" value="Submit!" style={{ width: '100%', borderRadius: '20px', background: '#2e75b6', marginTop: '10%' }} />{' '}
+            </Form>
+          </LoginFormRight>
+        </LoginFormDiv>
+      </ContainerDiv>
+      <>
+        <FooterDiv className="bg-gray">
+          <Navbar>
+            <Container>
+              <Footer>
+                <FooterTextLayout>©WALAB 2022 </FooterTextLayout>
+                <FooterTextLayout> 안병웅, 이선경, 김주은</FooterTextLayout>
+              </Footer>
+            </Container>
+          </Navbar>
+        </FooterDiv>
+      </>
+    </>
   );
 }
 
@@ -84,14 +106,14 @@ const BREAK_POINT_PC = 1200;
 
 const ContainerDiv = styled.div`
   width: 100vw;
-  height: 100vh;
+  height: 90vh;
   background: #eff0f2;
   display: flex;
   justify-content: center;
   align-items: center;
   background-image: url(${background});
 
-  width: ${({ xs }) => (xs ? `${calcWidthPercent(xs)}%` : `100%`)};
+  width: ${({ xs }) => (xs ? `${calcWidthPercent(xs)}%` : `100vw`)};
   @media only screen and (min-width: ${BREAK_POINT_MOBILE}px) {
     width: ${({ sm }) => sm && `${calcWidthPercent(sm)}%`};
   }
@@ -105,7 +127,7 @@ const ContainerDiv = styled.div`
 const LoginFormDiv = styled.div`
   background: #f5f5f5;
   width: 1050px;
-  height: 700px;
+  height: auto;
   display: flex;
   flex-direction: row;
   box-shadow: 10px black;
@@ -117,13 +139,13 @@ const LoginFormDiv = styled.div`
   box-shadow: 27px 43px 43px -26px rgba(89, 89, 89, 0.39);
 
   width: ${({ xs }) => (xs ? `${calcWidthPercent(xs)}%` : `70vw`)};
-  @media only screen and (min-width: ${BREAK_POINT_MOBILE}px) {
+  @media only screen and (max-width: ${BREAK_POINT_MOBILE}px) {
     width: ${({ sm }) => sm && `${calcWidthPercent(sm)}%`};
   }
-  @media only screen and (min-width: ${BREAK_POINT_TABLET}px) {
+  @media only screen and (max-width: ${BREAK_POINT_TABLET}px) {
     width: ${({ md }) => md && `${calcWidthPercent(md)}%`};
   }
-  @media only screen and (min-width: ${BREAK_POINT_PC}px) {
+  @media only screen and (max-width: ${BREAK_POINT_PC}px) {
     width: ${({ lg }) => lg && `${calcWidthPercent(lg)}%`};
   }
 `;
@@ -134,17 +156,17 @@ const LoginFormLeft1 = styled.div`
   display: flex;
   background: #2e75b6;
   align-items: center;
-  color: #2e75b6;
 
   width: ${({ xs }) => (xs ? `${calcWidthPercent(xs)}%` : `5%`)};
   padding: 1%;
-  @media only screen and (min-width: ${BREAK_POINT_MOBILE}px) {
+  @media only screen and (max-width: ${BREAK_POINT_MOBILE}px) {
     width: ${({ sm }) => sm && `${calcWidthPercent(sm)}%`};
+    display: none;
   }
-  @media only screen and (min-width: ${BREAK_POINT_TABLET}px) {
+  @media only screen and (max-width: ${BREAK_POINT_TABLET}px) {
     width: ${({ md }) => md && `${calcWidthPercent(md)}%`};
   }
-  @media only screen and (min-width: ${BREAK_POINT_PC}px) {
+  @media only screen and (max-width: ${BREAK_POINT_PC}px) {
     width: ${({ lg }) => lg && `${calcWidthPercent(lg)}%`};
   }
 `;
@@ -158,13 +180,14 @@ const LoginFormLeft2 = styled.div`
   color: #797979;
 
   width: ${({ xs }) => (xs ? `${calcWidthPercent(xs)}%` : `70vw`)};
-  @media only screen and (min-width: ${BREAK_POINT_MOBILE}px) {
+  @media only screen and (max-width: ${BREAK_POINT_MOBILE}px) {
     width: ${({ sm }) => sm && `${calcWidthPercent(sm)}%`};
+    display: none;
   }
-  @media only screen and (min-width: ${BREAK_POINT_TABLET}px) {
+  @media only screen and (max-width: ${BREAK_POINT_TABLET}px) {
     width: ${({ md }) => md && `${calcWidthPercent(md)}%`};
   }
-  @media only screen and (min-width: ${BREAK_POINT_PC}px) {
+  @media only screen and (max-width: ${BREAK_POINT_PC}px) {
     width: ${({ lg }) => lg && `${calcWidthPercent(lg)}%`};
   }
 `;
@@ -205,4 +228,18 @@ const LoginFormRightTitle = styled.div`
   display: flex;
   justify-content: center;
   padding: 60% 0px 10px 0px;
+`;
+const FooterDiv = styled.div`
+  display: flex;
+  align-itmes: center;
+  justify-content: center;
+`;
+const Footer = styled.div`
+  display: flex;
+  align-itmes: center;
+`;
+const FooterTextLayout = styled.div`
+  color: gray;
+  font-size: 14px;
+  padding: 0.5rem;
 `;
