bffc5d5417d70d6ca9d16856d6b333f1c00a1628
Lee <21801049@handong.edu>
2022-03-31 16:47:24 +0900

    [#66] 회원가입 페이지 반응형, 헤더 적용

--- FrontEnd/csee-selfchecker/src/pages/Register/index.js
+++ FrontEnd/csee-selfchecker/src/pages/Register/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
-import background from '../../asset/img/backgroundImg.png';
 import logo from '../../asset/img/loginImage.png';
+import background from '../../asset/img/backgroundImg.png';
+import headerImg from '../../asset/img/csee-logo-symbol.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Form } from 'react-bootstrap';
+import { Form, Navbar, Container, Button } from 'react-bootstrap';
 import { useNavigate, useLocation } from 'react-router';
 
 export default function Resister() {
@@ -42,40 +43,61 @@ export default function Resister() {
   };
 
   return (
-    <ContainerDiv>
-      <LoginFormDiv>
-        <LoginFormLeft1 />
-        <LoginFormLeft2>
-          <LoginFormLeftTitle>
-            Computer Science <br></br>And Electrical Engineering
-          </LoginFormLeftTitle>
-          <LoginFormLeftsubTitle>Welcome to our website</LoginFormLeftsubTitle>
-          <LogoImg src={logo} />
-        </LoginFormLeft2>
+    <>
+      <Navbar bg="light">
+        <Container>
+          <Navbar.Brand href="/">
+            <img alt="" src={headerImg} width="30" height="30" className="d-inline-block align-top" /> CSEE Self-Checker
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+      <ContainerDiv>
+        <LoginFormDiv>
+          <LoginFormLeft1 />
+          <LoginFormLeft2>
+            <LoginFormLeftTitle>
+              Computer Science <br></br>And Electrical Engineering
+            </LoginFormLeftTitle>
+            <LoginFormLeftsubTitle>Welcome to our website</LoginFormLeftsubTitle>
+            <LogoImg src={logo} />
+          </LoginFormLeft2>
 
-        <LoginFormRight>
-          <Form onSubmit={handleSubmit}>
-            <LoginFormRightTitle>
-              <h3>Student Information</h3>
-            </LoginFormRightTitle>
-            <Form.Group className="mb-3" style={{ marginTop: '50px' }}>
-              <Form.Control placeholder="  학번 (Student ID)" name="studentId" onChange={handleChange} style={{ borderRadius: '20px' }} />
-            </Form.Group>
-            <Form.Group className="mb-3">
-              <Form.Control placeholder="  이름 (Name)" name="studentName" onChange={handleChange} style={{ borderRadius: '20px' }} />
-            </Form.Group>
-            <Form.Group className="mb-3">
-              <Form.Control placeholder="  핸드폰 뒤 4자리 (XXXX)" name="studentPhone" onChange={handleChange} style={{ borderRadius: '20px' }} />
-              <p style={{ marginTop: '2vh', fontSize: '1.2vw', textAlign: 'center' }}>Hisnet에 저장된 연락처 뒤 4자리를 입력하세요.</p>
-            </Form.Group>
-            <Button as="input" type="submit" value="회원가입" style={{ width: '100%', borderRadius: '20px', background: '#2e75b6', marginTop: '10%' }} />{' '}
-            <p style={{ marginTop: '2vh', fontSize: '1.2vw', textAlign: 'center' }}>
-              회원가입이 안될 시 관리자에게 문의하세요.<br></br>[054-260-1234 / admin@handong.ac.kr]{' '}
-            </p>
-          </Form>
-        </LoginFormRight>
-      </LoginFormDiv>
-    </ContainerDiv>
+          <LoginFormRight>
+            <Form onSubmit={handleSubmit}>
+              <LoginFormRightTitle>
+                <h3>Student Information</h3>
+              </LoginFormRightTitle>
+              <Form.Group className="mb-3" style={{ marginTop: '50px' }}>
+                <Form.Control placeholder="  학번 (Student ID)" name="studentId" onChange={handleChange} style={{ borderRadius: '20px' }} />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Control placeholder="  이름 (Name)" name="studentName" onChange={handleChange} style={{ borderRadius: '20px' }} />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Control placeholder="  핸드폰 뒤 4자리 (XXXX)" name="studentPhone" onChange={handleChange} style={{ borderRadius: '20px' }} />
+                <p style={{ marginTop: '2vh', fontSize: '1.2vw', textAlign: 'center' }}>Hisnet에 저장된 연락처 뒤 4자리를 입력하세요.</p>
+              </Form.Group>
+              <Button as="input" type="submit" value="회원가입" style={{ width: '100%', borderRadius: '20px', background: '#2e75b6', marginTop: '10%' }} />{' '}
+              <p style={{ marginTop: '2vh', fontSize: '1.2vw', textAlign: 'center' }}>
+                회원가입이 안될 시 관리자에게 문의하세요.<br></br>[054-260-1234 / admin@handong.ac.kr]{' '}
+              </p>
+            </Form>
+          </LoginFormRight>
+        </LoginFormDiv>
+      </ContainerDiv>
+      <>
+        <FooterDiv className="bg-gray">
+          <Navbar>
+            <Container>
+              <Footer>
+                <FooterTextLayout>©WALAB 2022 </FooterTextLayout>
+                <FooterTextLayout> 안병웅, 이선경, 김주은</FooterTextLayout>
+              </Footer>
+            </Container>
+          </Navbar>
+        </FooterDiv>
+      </>
+    </>
   );
 }
 
@@ -91,14 +113,14 @@ const BREAK_POINT_PC = 1200;
 
 const ContainerDiv = styled.div`
   width: 100vw;
-  height: 100vh;
+  height: 90vh;
   background: #eff0f2;
   display: flex;
   justify-content: center;
   align-items: center;
   background-image: url(${background});
 
-  width: ${({ xs }) => (xs ? `${calcWidthPercent(xs)}%` : `100%`)};
+  width: ${({ xs }) => (xs ? `${calcWidthPercent(xs)}%` : `100vw`)};
   @media only screen and (min-width: ${BREAK_POINT_MOBILE}px) {
     width: ${({ sm }) => sm && `${calcWidthPercent(sm)}%`};
   }
@@ -112,7 +134,7 @@ const ContainerDiv = styled.div`
 const LoginFormDiv = styled.div`
   background: #f5f5f5;
   width: 1050px;
-  height: 700px;
+  height: auto;
   display: flex;
   flex-direction: row;
   box-shadow: 10px black;
@@ -124,13 +146,13 @@ const LoginFormDiv = styled.div`
   box-shadow: 27px 43px 43px -26px rgba(89, 89, 89, 0.39);
 
   width: ${({ xs }) => (xs ? `${calcWidthPercent(xs)}%` : `70vw`)};
-  @media only screen and (min-width: ${BREAK_POINT_MOBILE}px) {
+  @media only screen and (max-width: ${BREAK_POINT_MOBILE}px) {
     width: ${({ sm }) => sm && `${calcWidthPercent(sm)}%`};
   }
-  @media only screen and (min-width: ${BREAK_POINT_TABLET}px) {
+  @media only screen and (max-width: ${BREAK_POINT_TABLET}px) {
     width: ${({ md }) => md && `${calcWidthPercent(md)}%`};
   }
-  @media only screen and (min-width: ${BREAK_POINT_PC}px) {
+  @media only screen and (max-width: ${BREAK_POINT_PC}px) {
     width: ${({ lg }) => lg && `${calcWidthPercent(lg)}%`};
   }
 `;
@@ -141,17 +163,17 @@ const LoginFormLeft1 = styled.div`
   display: flex;
   background: #2e75b6;
   align-items: center;
-  color: #2e75b6;
 
   width: ${({ xs }) => (xs ? `${calcWidthPercent(xs)}%` : `5%`)};
   padding: 1%;
-  @media only screen and (min-width: ${BREAK_POINT_MOBILE}px) {
+  @media only screen and (max-width: ${BREAK_POINT_MOBILE}px) {
     width: ${({ sm }) => sm && `${calcWidthPercent(sm)}%`};
+    display: none;
   }
-  @media only screen and (min-width: ${BREAK_POINT_TABLET}px) {
+  @media only screen and (max-width: ${BREAK_POINT_TABLET}px) {
     width: ${({ md }) => md && `${calcWidthPercent(md)}%`};
   }
-  @media only screen and (min-width: ${BREAK_POINT_PC}px) {
+  @media only screen and (max-width: ${BREAK_POINT_PC}px) {
     width: ${({ lg }) => lg && `${calcWidthPercent(lg)}%`};
   }
 `;
@@ -165,13 +187,14 @@ const LoginFormLeft2 = styled.div`
   color: #797979;
 
   width: ${({ xs }) => (xs ? `${calcWidthPercent(xs)}%` : `70vw`)};
-  @media only screen and (min-width: ${BREAK_POINT_MOBILE}px) {
+  @media only screen and (max-width: ${BREAK_POINT_MOBILE}px) {
     width: ${({ sm }) => sm && `${calcWidthPercent(sm)}%`};
+    display: none;
   }
-  @media only screen and (min-width: ${BREAK_POINT_TABLET}px) {
+  @media only screen and (max-width: ${BREAK_POINT_TABLET}px) {
     width: ${({ md }) => md && `${calcWidthPercent(md)}%`};
   }
-  @media only screen and (min-width: ${BREAK_POINT_PC}px) {
+  @media only screen and (max-width: ${BREAK_POINT_PC}px) {
     width: ${({ lg }) => lg && `${calcWidthPercent(lg)}%`};
   }
 `;
@@ -211,5 +234,19 @@ const LoginFormRight = styled.div`
 const LoginFormRightTitle = styled.div`
   display: flex;
   justify-content: center;
-  padding: 18vh 0px 10px 0px;
+  padding: 20% 0px 10px 0px;
+`;
+const FooterDiv = styled.div`
+  display: flex;
+  align-itmes: center;
+  justify-content: center;
+`;
+const Footer = styled.div`
+  display: flex;
+  align-itmes: center;
+`;
+const FooterTextLayout = styled.div`
+  color: gray;
+  font-size: 14px;
+  padding: 0.5rem;
 `;
