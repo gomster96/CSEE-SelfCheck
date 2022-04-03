75a78511cfa79a1fd3dd6459b157798210a8af49
JuEun <86585240+kimjueun1@users.noreply.github.com>
2022-04-03 22:28:47 +0900

    Feat/#91 (#92)
    * [#65] feat: main, selfcheck css 수정
    
    * [#91] feat: main, selfCheck user table 반응형 구현
    
    * git merge
    
    * [#91] merge

---
+++ FrontEnd/csee-selfchecker/src/pages/SelfCheck/components/TableRow.js
@@ -0,0 +1,8 @@
+// TableRow.js
+import { StyledTableRow } from "./Table_styled";
+
+const TableRow = ({ children }) => {
+  return <StyledTableRow>{children}</StyledTableRow>;
+};
+
+export default TableRow;
