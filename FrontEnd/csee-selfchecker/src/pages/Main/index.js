228b0f64953ec75a8a38eaf70ff616b3009ece2c
JuEun <moomin@handong.ac.kr>
2022-04-03 22:38:34 +0900

    [#93] refactor: Main index파일 재업로드

--- FrontEnd/csee-selfchecker/src/pages/Main/index.js
+++ FrontEnd/csee-selfchecker/src/pages/Main/index.js
@@ -1,55 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { ContainerDiv, RoundBackgroundDiv, InnerLayout, TableLayout, TextLayout, ButtonStyle, FooterDiv, Footer, FooterTextLayout } from './main.styled';
+import MypageTable from './MypageTable';
 import Table from './components/Table';
-import SelfCheckTable from './SelfCheckTable';
-import SelfCheckResultTable from './SelfCheckResultTable';
-import background from '../../asset/img/backgroundImg.png';
+import MypageResultTable from './MypageResultTable';
+
 import headerImg from '../../asset/img/csee-logo-symbol.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useNavigate, useLocation } from 'react-router';
 import { Navbar, Nav, NavDropdown, Form, Button, Container, FormControl } from 'react-bootstrap';
 
-export default function Main() {
-  return (
-    <>
-      <Navbar bg="light">
-        <Container>
-          <Navbar.Brand href="/">
-            <img alt="" src={headerImg} width="30" height="30" className="d-inline-block align-top" /> CSEE Self-Checker
-          </Navbar.Brand>
-        </Container>
-      </Navbar>
-      <ContainerDiv>
-        <RoundBackgroundDiv>
-          <InnerLayout>
-            <TableLayout>
-              <h1>Self Check</h1>
-            </TableLayout>
-
-            <Table />
-
-            <TableLayout>
-              <SelfCheckResultTable />
-            </TableLayout>
-            <ButtonStyle>
-              <Button className="rounded-pill m-2">초기화</Button>
-              <Button className="rounded-pill m-2">저장하기</Button>
-            </ButtonStyle>
-          </InnerLayout>
-        </RoundBackgroundDiv>
-      </ContainerDiv>
+class index extends Component {
+  render() {
+    return (
       <>
-        <FooterDiv className="bg-gray">
-          <Navbar>
-            <Container>
-              <Footer>
-                <FooterTextLayout>©WALAB 2022 </FooterTextLayout>
-                <FooterTextLayout> 안병웅, 이선경, 김주은</FooterTextLayout>
-              </Footer>
-            </Container>
-          </Navbar>
-        </FooterDiv>
+        <Navbar bg="light">
+          <Container>
+            <Navbar.Brand href="/">
+              <img alt="" src={headerImg} width="30" height="30" className="d-inline-block align-top" /> CSEE Self-Checker
+            </Navbar.Brand>
+          </Container>
+        </Navbar>
+        <ContainerDiv>
+          <RoundBackgroundDiv>
+            <InnerLayout>
+              <TableLayout>
+                <h1>Mypage</h1>
+              </TableLayout>
+
+              <Table />
+
+              <TableLayout>
+                <MypageResultTable />
+              </TableLayout>
+              <TextLayout>
+                <h4>
+                  공학프로젝트 입문 수강이 <span className="text-danger">가능</span>합니다
+                </h4>
+              </TextLayout>
+
+              <ButtonStyle>
+                <Button className="rounded-pill m-2">수정하기</Button>
+              </ButtonStyle>
+            </InnerLayout>
+          </RoundBackgroundDiv>
+        </ContainerDiv>
+        <>
+          <FooterDiv className="bg-gray">
+            <Navbar>
+              <Container>
+                <Footer>
+                  <FooterTextLayout>©WALAB 2022 </FooterTextLayout>
+                  <FooterTextLayout> 안병웅, 이선경, 김주은</FooterTextLayout>
+                </Footer>
+              </Container>
+            </Navbar>
+          </FooterDiv>
+        </>
       </>
-    </>
-  );
+    );
+  }
 }
+
+export default index;
