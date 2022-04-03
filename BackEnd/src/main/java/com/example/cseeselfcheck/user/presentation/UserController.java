191e0d2701de40aaecce20d203ba2699ed9c6f0d
JuEun <moomin@handong.ac.kr>
2022-04-04 07:40:36 +0900

    [#95] feat: data fetch 구현중

--- BackEnd/src/main/java/com/example/cseeselfcheck/user/presentation/UserController.java
+++ BackEnd/src/main/java/com/example/cseeselfcheck/user/presentation/UserController.java
@@ -22,6 +22,7 @@ import javax.servlet.http.HttpSession;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
+@CrossOrigin(origins = "*")
 public class UserController {
 
     private final ReferenceUserService referenceUserService;
