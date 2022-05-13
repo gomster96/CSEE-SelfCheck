8d2c349f4c6d842f211eae2e520839ef7a698fdc
gomster <gomster96@gmail.com>
2022-05-14 03:44:35 +0900

    feat[#77] : excel export API 기능 구현

--- BackEnd/src/main/java/com/example/cseeselfcheck/user/application/UserService.java
+++ BackEnd/src/main/java/com/example/cseeselfcheck/user/application/UserService.java
@@ -1,7 +1,6 @@
 package com.example.cseeselfcheck.user.application;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import com.example.cseeselfcheck.admin.presentation.dto.AdminUserRequestDto;
@@ -39,42 +38,43 @@ public class UserService {
     private final MajorRepository majorRepository;
 
     public Optional<User> checkUserByEmail(String email) {
-        System.out.println(email+" 중복검사");
+        System.out.println(email + " 중복검사");
         return userRepository.findByEmail(email);
     }
+
     @Transactional(readOnly = true)
     public List<AdminUserResponseDto> getFilteredUser(AdminUserRequestDto data) {
 
         List<UserDataDto> userDatas = userRepository.findUserData(data.getSearchWord());
         return userDatas.stream()
-                .filter(data::isUserContainLecture)
-                .filter(data::isUserContainSemester)
-                .filter(data::isSameTakePossible)
-                .map(AdminUserResponseDto::new)
-                .collect(Collectors.toList());
+                        .filter(data::isUserContainLecture)
+                        .filter(data::isUserContainSemester)
+                        .filter(data::isSameTakePossible)
+                        .map(AdminUserResponseDto::new)
+                        .collect(Collectors.toList());
     }
 
     @Transactional(readOnly = true)
     public UserFullDataResponseDto getUserIndividualDataById(Long userId) {
         UserIndividualDataDto individualData = userRepository.findFirstByIndividualDataById(userId)
-                .orElseThrow();
+                                                             .orElseThrow();
         String lectureChecker = individualData.getChecker();
         List<Lecture> lectures = lectureRepository.findAll();
         List<LectureDataDto> userLectures = lectures.stream()
-                .filter(lecture -> lecture.isMandatoryLecture(lectureChecker))
-                .map(LectureDataDto::new)
-                .collect(Collectors.toList());
+                                                    .filter(lecture -> lecture.isMandatoryLecture(lectureChecker))
+                                                    .map(LectureDataDto::new)
+                                                    .collect(Collectors.toList());
         return new UserFullDataResponseDto(individualData, userLectures);
     }
 
     @Transactional
-    public UserDataSaveResponseDto saveUserData(UserDataSaveRequest userSaveData){
+    public UserDataSaveResponseDto saveUserData(UserDataSaveRequest userSaveData) {
         User user = userRepository.findById(userSaveData.getUserId()).orElseThrow();
         Major userMajor = majorRepository.findFirstByStudentNumber(user.getStudentNumber()).orElseThrow();
         String checker = userMajor.getChecker();
         int result = 1;
-        for(int i=0; i<checker.length(); i++){
-            if(checker.charAt(i)=='1'&&  userSaveData.getTakenStatus().charAt(i) == '0'){
+        for (int i = 0; i < checker.length(); i++) {
+            if (checker.charAt(i) == '1' && userSaveData.getTakenStatus().charAt(i) == '0') {
                 result = 0;
                 break;
             }
@@ -87,7 +87,7 @@ public class UserService {
     public List<UserResponseDto> checkUserInfo(UserCheckRequestDto data) {
         List<UserCheckDto> userDatas = referenceUserRepository.findByStudentNumber(data.getStudentNumber());
         User user = new User();
-        if(userDatas.isEmpty()) {
+        if (userDatas.isEmpty()) {
             System.out.println("체크 요청 실패!");
             return null;
         } else {
@@ -96,8 +96,37 @@ public class UserService {
             userRepository.save(user);
             System.out.println("회원가입 성공!");
             return userDatas.stream()
-                    .map(UserResponseDto::new)
-                    .collect(Collectors.toList());
+                            .map(UserResponseDto::new)
+                            .collect(Collectors.toList());
         }
+
+
+    }
+
+    public Map<String, Object> getUserExcelData(AdminUserRequestDto data) {
+        List<AdminUserResponseDto> userDatas = getFilteredUser(data);
+        List<Lecture> lectures = lectureRepository.findAll();
+        Map<String, Object> excelMap = new HashMap<>();
+        excelMap.put("filename", "CSEE_Checker_Student");
+        excelMap.put("head", Arrays.asList("학번", "이름", "학기", "전공", "판정", "수강상태"));
+        List<Object> excelBodyDatas = userDatas.stream()
+                                               .map(bodyData -> Arrays.asList(bodyData.getStudentNumber(), bodyData.getName(), bodyData.getSemester(), bodyData.getMajorName(), bodyData.getResult() == 1 ? "가능" : "불가능", parsetakenStatusToLecture(lectures, bodyData.getTakenStatus())))
+                                               .collect(Collectors.toList());
+        excelMap.put("body", excelBodyDatas);
+        return excelMap;
+    }
+
+    private String parsetakenStatusToLecture(List<Lecture> lectures, String takenStatus) {
+        StringBuilder sb = new StringBuilder("");
+        for (Lecture lecture : lectures) {
+            if (takenStatus.charAt(lecture.getLecturePosition()) == '-')
+                sb.append(lecture.getLectureName()).append(", ");
+            if (takenStatus.charAt(lecture.getLecturePosition()) == '+')
+                sb.append(lecture.getLectureName()).append(" 병수예정, ");
+        }
+        if (sb.length() > 2)
+            sb.delete(sb.length() - 2, sb.length());
+        return sb.toString();
+
     }
 }
