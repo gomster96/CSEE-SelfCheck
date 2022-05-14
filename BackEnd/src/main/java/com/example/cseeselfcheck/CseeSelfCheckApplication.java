a7bdd350c0529b4fbe2324a407a11f68d7652d69
gomster <gomster96@gmail.com>
2022-05-14 13:59:11 +0900

    test

--- BackEnd/src/main/java/com/example/cseeselfcheck/CseeSelfCheckApplication.java
+++ BackEnd/src/main/java/com/example/cseeselfcheck/CseeSelfCheckApplication.java
@@ -2,14 +2,20 @@ package com.example.cseeselfcheck;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class CseeSelfCheckApplication {
+public class CseeSelfCheckApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(CseeSelfCheckApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        return super.configure(builder);
+    }
 }
